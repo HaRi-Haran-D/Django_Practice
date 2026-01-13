ec4fba51a8bbd7227122212ea7b9fa7378a8a7ff
HaRi-Haran-D <hvj58128@gmail.com>
2026-01-13 17:20:30 +0530

    Django Practice

---
+++ demo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50, blank=False, unique=True)
+    description = models.TextField(blank=True, max_length=250)
+    price = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+    published = models.DateField(blank=True, null=True, default=None)
+    cover = models.ImageField(upload_to='covers/', blank=True)
+    is_published = models.BooleanField(default=False)
+    #author = models.CharField(max_length=50)
---
+++ demo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Book
+
+# Create your views here.
+
+def first(request):
+    books = Book.objects.all()
+    return render(request, 'first_temp.html',{'books': books})
